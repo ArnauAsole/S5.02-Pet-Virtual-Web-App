8d529ea944b14fba8d56bae806d20dad888dc234
arnau <solenueza@gmail.com>
2025-09-26 14:05:06 +0200

    New Project started(Pet)

---
+++ src/main/java/com/tolkien/pets/TolkienVirtualPetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.tolkien.pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolkienVirtualPetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolkienVirtualPetsApiApplication.class, args);
+	}
+
+}
